312de7ae0b11d9e2824a12b375db3318832f6037
d.voitekh <d.voitekh@gmail.com>
2017-08-29 17:51:09 +0300

    added rewire package

---
+++ config-overrides.js
@@ -0,0 +1,8 @@
+module.exports = function override(config, env) {
+  if (env == 'production') {
+    config.devtool = false;
+    config.plugins.splice(5, 2); // remove ManifestPlugin and SWPrecacheWebpackPlugin
+  }
+
+  return config;
+};
